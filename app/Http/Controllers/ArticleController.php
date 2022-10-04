a361869ab91a36e7a151e8b5d60ca8cbcf4b6c4b
xristosD <xristos567@gmail.com>
2022-10-04 20:11:52 +0300

    Creates a route to article index

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        $articles = Article::query()->where('published', true)->get();
+
+        return view('dashboard', [
+            'articles' => $articles,
+        ]);
+    }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,20 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      {{ __('Dashboard') }}
+    </h2>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
-        </div>
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        @foreach ($articles as $article)
+         <div>{{ $article->title }}</div>
+         <div>{{ $article->created_at }}</div>
+         <div>{{ $article->user->name }}</div>
+         <div>{{ $article->user->name }}</div>
+        @endforeach
+      </div>
     </div>
+  </div>
 </x-app-layout>
