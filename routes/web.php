cafb3ef37b0adf500f9a1f028bd4834aff6b140c
xristosD <xristos567@gmail.com>
2022-10-07 11:01:04 +0300

    Refactoring Nav styles

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ArticleController::class, 'index']);
+Route::get('/', [ArticleController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
