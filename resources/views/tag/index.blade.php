1daf697a0db4d49cb0ae4f9aedfd8292bd982c8c
xristosD <xristos567@gmail.com>
2022-10-24 18:55:29 +0300

    Tasks index page should use JS to render elements

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -7,11 +7,42 @@
 
   <div class="py-12">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-      <div id="tagcontainer" class="overflow-hidden flex flex-col px-1 md:px-5 md:py-10">
-        @foreach ( $tags as $tag )
-          <div>{{$tag->title}}</div>
-        @endforeach
+      <div id="tagContainer" class="overflow-hidden px-1 md:px-5 md:py-10" data-tags={{$tags}}>
+        <h2 id="charHeader" class="font-medium text-xl pb-2 pl-1"></h2>
+        <div id="tagGroup" class="flex flex-wrap gap-2 pb-5">
+          <a id="tagEl" class="btn btn-outline btn-sm " href=""></a>
+        </div>
       </div>
     </div>
   </div>
+
+  <x-slot name="footerScripts">
+    <script>
+      const tagContainer = document.getElementById("tagContainer");
+      const sortedTags = JSON.parse( tagContainer.dataset.tags );
+      groupedTags = sortedTags.reduce((prev, tag) => {
+        if(!(Array.from(tag.title)[0] in prev)) { prev[Array.from(tag.title)[0]] = [] }
+        prev[Array.from(tag.title)[0]].push(tag);
+        return prev;
+      }, {})
+      for( charGroup in groupedTags ) {
+        let charHeader = document.getElementById("charHeader").cloneNode(false);
+        charHeader.removeAttribute('id');
+        charHeader.textContent = charGroup;
+        tagContainer.appendChild(charHeader)
+        let tagGroup = document.getElementById('tagGroup').cloneNode(false);
+        tagGroup.removeAttribute('id');
+        groupedTags[charGroup].forEach(tag => {
+          let tagEl = document.getElementById('tagEl').cloneNode(false);
+          tagEl.removeAttribute('id');
+          tagEl.href = "/tags/" + tag.id;
+          tagEl.textContent = '#' + tag.title;
+          tagGroup.appendChild(tagEl);
+        });
+        tagContainer.appendChild(tagGroup);
+      }
+      tagContainer.removeChild(document.getElementById("charHeader"));
+      tagContainer.removeChild(document.getElementById("tagGroup"));
+    </script>
+  </x-slot>
 </x-app-layout>
